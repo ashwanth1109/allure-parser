ab4a70393418b37774c2c1751906cf316c4bbe6b
Ashwanth A R <ashwanth1109@gmail.com>
2020-02-03 12:07:02 +0530

    Feature: File Upload Enhancement: Sorted Report

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useCallback, useRef } from 'react';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
 import * as R from 'ramda';
 import ReactJSON from 'react-json-view';
-import categories from '../data/categories43';
 
 const pages = {
   HOME: 'HOME',
@@ -10,89 +9,95 @@ const pages = {
 };
 
 export default () => {
-  const inputRef = useRef(null);
-  const textAreaRef = useRef(null);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(null);
   const [page, setPage] = useState(pages.HOME);
   const [failingTests, setFailingTests] = useState({});
-  const [rootCauses, setRootCauses] = useState({});
+  // const [rootCauses, setRootCauses] = useState({});
 
   const showReport = useCallback(() => {
     const tests = {};
     // TODO: Figure out how to parse the file to generate report
-    const prodDefects = categories.children[0].children;
-    console.log(prodDefects);
+    const prodDefects = file.children[0].children;
     prodDefects.map(test => {
-      tests[test.name] = test.children.reduce((acc, val) => {
+      let testName;
+      if (test.name.includes('Wait timed out after ')) {
+        testName = test.name.split('Wait timed out after ')[0];
+      } else {
+        testName = test.name;
+      }
+      tests[testName] = test.children.reduce((acc, val) => {
         return [...acc, val.name];
-      }, []);
+      }, tests[testName] || []);
     });
-    setFailingTests(tests);
+    const testArr = [];
+    R.mapObjIndexed((val, key) => {
+      testArr.push([key, val]);
+    }, tests);
+    const sortedArr = R.sort((a, b) => b[1].length - a[1].length, testArr);
+    const sortedTests = {};
+    R.map(val => {
+      sortedTests[val[0]] = val[1];
+    }, sortedArr);
+    setFailingTests(sortedTests);
     setPage(pages.REPORT);
+  }, [file]);
+
+  // const addRootCause = useCallback(() => {
+  //   const { value: cause } = inputRef.current;
+  //   rootCauses[cause] = [];
+  //   const newFailingTests = {};
+  //   Object.keys(failingTests).map(key => {
+  //     const tests = failingTests[key];
+  //     if (key.includes(cause)) {
+  //       rootCauses[cause] = rootCauses[cause] = [
+  //         ...rootCauses[cause],
+  //         ...tests
+  //       ];
+  //     } else {
+  //       newFailingTests[key] = tests;
+  //     }
+  //   });
+  //   setFailingTests(newFailingTests);
+  //   inputRef.current.value = '';
+  // }, [failingTests]);
+
+  const getFile = useCallback(e => {
+    e.preventDefault();
+    const file = fileRef.current.files[0];
+    const reader = new FileReader();
+    reader.onload = e => {
+      setFile(JSON.parse(e.target.result));
+    };
+    reader.readAsText(file);
   }, []);
 
-  const showCauses = useCallback(() => {
-    // TODO: Sort this object
-    // R.map(rootCauses)
-    setPage(pages.CAUSES);
-  }, [rootCauses]);
-
-  const addRootCause = useCallback(() => {
-    const { value: cause } = inputRef.current;
-    rootCauses[cause] = [];
-    const newFailingTests = {};
-    Object.keys(failingTests).map(key => {
-      const tests = failingTests[key];
-      if (key.includes(cause)) {
-        rootCauses[cause] = rootCauses[cause] = [
-          ...rootCauses[cause],
-          ...tests
-        ];
-      } else {
-        newFailingTests[key] = tests;
-      }
-    });
-    setFailingTests(newFailingTests);
-    inputRef.current.value = '';
-  }, [failingTests]);
+  useEffect(() => {
+    if (file) showReport();
+  }, [file]);
 
   if (page === pages.HOME)
     return (
       <div className="col container">
         <div className="app-header">
-          <h2>Paste the categories.json file below</h2>
-          <button onClick={showReport}>Generate Report</button>
+          <form onSubmit={getFile}>
+            <h2>Upload your Categories.json here</h2>
+            <input type="file" name="myFile" ref={fileRef} />
+            <button type="submit">Upload & Generate Report</button>
+          </form>
         </div>
-        <textarea
-          placeholder="Enter your file contents here"
-          data-gramm_editor="false"
-          ref={textAreaRef}
-        />
       </div>
     );
 
   if (page === pages.REPORT)
     return (
       <div className="col container">
-        <div className="app-header">
-          <input type="text" placeholder="Create a root cause" ref={inputRef} />
-          <button onClick={addRootCause}>Add</button>
-          <button onClick={showCauses}>View</button>
-        </div>
+        {/*<div className="app-header">*/}
+        {/*  <input type="text" placeholder="Create a root cause" ref={inputRef} />*/}
+        {/*  <button onClick={addRootCause}>Add</button>*/}
+        {/*</div>*/}
         <div className="monokai-bg">
           <ReactJSON src={failingTests} theme="monokai" collapsed={1} />
-        </div>
-      </div>
-    );
-
-  if (page === pages.CAUSES)
-    return (
-      <div className="col container m-top-32">
-        <div className="app-header">
-          <button onClick={() => setPage(pages.REPORT)}>Go Back</button>
-          <h2>Root Causes</h2>
-        </div>
-        <div className="monokai-bg">
-          <ReactJSON src={rootCauses} theme="monokai" collapsed={1} />
         </div>
       </div>
     );
